03536a506eb8424df942abb189aa45d72f0f85ca
Andrew Heine <drewheine@gmail.com>
2022-05-19 13:23:26 -0400

    projectile collides with player

--- Smash Bros/Player.py
+++ Smash Bros/Player.py
@@ -6,7 +6,8 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 48)
 
 class Player:
-    def __init__(self, x, y, color):
+    def __init__(self, num, x, y, color):
+        self.num = num
         self.x = x
         self.y = y
         self.vx = 0
@@ -14,7 +15,7 @@ class Player:
         self.facing = 1
         self.name = "Temporary"
         self.jumps = 0
-        self.height = 1
+        self.radius = 1
         self.health = 100
         self.left = False
         self.right = False
@@ -24,25 +25,25 @@ class Player:
         self.lastShot = 0
         self.projectiles = []
 
-    def update(self, level, dt):
+    def update(self, level, players, dt):
         if self.health <= 0:
             return
 
         for projectile in self.projectiles:
             if projectile.alive == True:
-                projectile.update(level,dt)
+                projectile.update(level,players,dt)
 
         self.vy += 12 * dt
 
         for obstacle in level.obstacles:
             if self.vy > 0.0 and obstacle.tileType > 0:
-                yMatch = obstacle.y + .4 > (self.y + self.height) > obstacle.y - .4
+                yMatch = obstacle.y + .4 > (self.y + self.radius) > obstacle.y - .4
                 xMatch = obstacle.x <= self.x <= obstacle.x + obstacle.width
 
                 if yMatch and xMatch:
                     self.jumps = 2
                     self.vy = 0
-                    self.y = obstacle.y - self.height
+                    self.y = obstacle.y - self.radius
 
         self.y += self.vy * dt
         self.x += self.vx * dt
@@ -51,7 +52,7 @@ class Player:
     def checkCollisions(self, projectiles):
         otherProjectiles = list(set(projectiles) - set(self.projectiles)) + list(set(self.projectiles) - set(projectiles))
         for projectile in otherProjectiles:
-                yMatch = projectile.y + .4 > (self.y + self.height) > projectile.y - .4
+                yMatch = projectile.y + .4 > (self.y + self.radius) > projectile.y - .4
                 xMatch = projectile.x <= self.x <= projectile.x + 1
                 if yMatch and xMatch:
                     print("hit")
@@ -67,6 +68,9 @@ class Player:
     def goRight(self):
         self.vx = 8
         self.facing = 1
+
+    def getRect(self):
+        return pygame.Rect(self.x,self.y, 2, 2)
     
     def move(self, dir):
         if (dir == -1):
@@ -80,7 +84,6 @@ class Player:
         self.vx *= 0.8
 
     def jump(self):
-        print(self.jumps)
         if self.jumps > 0:
             self.vy = -8
             self.jumps -=1
@@ -89,7 +92,7 @@ class Player:
         now = time.time()
         if now - self.lastShot > .8:
             self.lastShot = now
-            self.projectiles.append(Projectile(self.facing, self.x, self.y, 20 + abs(self.vx)))
+            self.projectiles.append(Projectile(self.num, self.facing, self.x, self.y, 5 + abs(self.vx)))
             if len(self.projectiles) > 10:
                 self.projectiles.pop(0)
 
@@ -99,11 +102,14 @@ class Player:
                 if abs(player.x - self.x) < 10 and abs(player.y - self.y) < 10:
                     player.health -= 10
 
+    def hit(self, damage):
+        self.health -= damage
+
     def draw(self, screen, pixelSize, playerIndex):
         if self.health<=0:
-            pygame.draw.circle(screen, (0,0,0),(self.x * pixelSize,self.y * pixelSize), self.height * pixelSize)
+            pygame.draw.circle(screen, (0,0,0),(self.x * pixelSize,self.y * pixelSize), self.radius * pixelSize)
         else:
-            pygame.draw.circle(screen, self.color,(self.x * pixelSize,self.y * pixelSize), self.height * pixelSize)
+            pygame.draw.circle(screen, self.color,(self.x * pixelSize,self.y * pixelSize), self.radius * pixelSize)
         for projectile in self.projectiles:
             if projectile.alive == True:
                 projectile.draw(screen)
--- Smash Bros/Projectile.py
+++ Smash Bros/Projectile.py
@@ -1,34 +1,43 @@
 import pygame
-import Level
+import Util
 
 class Projectile:
-    def __init__(self, direction, x, y, vx, pixelSize=20,  color = (255,0,0)):
+    def __init__(self, playerNum, direction, x, y, vx, pixelSize=20,  color = (255,0,0)):
         self.direction = direction
-        self.x = x
-        self.y = y
         self.vx = vx * direction
         self.pixelSize = pixelSize
         self.color = color
         self.alive = True
-        self.rect = pygame.Rect(self.x*pixelSize,self.y*pixelSize,pixelSize, pixelSize/2)
+        self.x = x
+        self.y = y
+        self.radius = .3
         self.collisionCheckCounter = 0
+        self.dmg = 5
+        self.playerNum = playerNum
     
-    def checkCollision(self, level):
+    def checkCollision(self, level, players):
         self.collisionCheckCounter += 1
         if self.collisionCheckCounter % 3 > 0:
             return
         if(self.x < 0 or self.x > 900 or self.y >1000):
             self.alive = False
-        for tangible in level.getTangible(): #this code lags a fuck ton 
-            if self.rect.colliderect(tangible):
+
+        for t in level.getTangible():
+            xCollision = (self.x > t.rect[0]) and (self.x < (t.rect[0] + t.rect[2]))
+            yCollision = (self.y > t.rect[1]) and (self.y < (t.rect[1] + t.rect[3]))
+            if xCollision and yCollision:
                 self.alive = False
 
+        for p in players:
+            if p.num != self.playerNum:
+                if Util.distance(p.getRect(), self) < self.radius + p.radius:
+                    p.health -= self.dmg
+                    self.alive = False
 
-    def update(self,level,dt):
-        self.rect.move_ip(self.vx*dt*self.pixelSize,0)
-        self.checkCollision(level)
+
+    def update(self,level, players, dt):
+        self.x += self.vx * dt * 1.5
+        self.checkCollision(level, players)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-    
-    
+        pygame.draw.circle(screen, self.color, (self.x * self.pixelSize,self.y * self.pixelSize), self.radius * self.pixelSize)
